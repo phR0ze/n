300adba91b10c9dbf78592a3c511516d750d5940
phR0ze <prc451@gmail.com>
2018-11-28 12:25:40 -0700

    Fix bug in contains

--- contains.go
+++ contains.go
@@ -5,9 +5,16 @@ import (
 	"strings"
 )
 
-// Contains checks if all of the given targets are found
-func (q *Queryable) Contains(targets interface{}) bool {
-	other := Q(targets)
+// Contains checks if all of the given obj are found.
+// When obj is a string and this is a string check will fall back on strings.Contains.
+// When obj is a string and this is a string slice, slice will be checked for obj.
+// When obj is a non-interable and this is non-iterable a direct check is made.
+// When obj is a non-interable and this is slice, slice will be checked for obj.
+// When obj is a slice of string and this is a string each string check using strings.Contains.
+// When obj is a slice and this is a slice each item will be checked in the slice.
+// When obj is a slice and this is a map each item will be checked in the map as a key.
+func (q *Queryable) Contains(obj interface{}) bool {
+	other := Q(obj)
 	if !q.Any() || !other.Any() {
 		return false
 	}
@@ -17,12 +24,12 @@ func (q *Queryable) Contains(targets interface{}) bool {
 
 		// Both strings - pass through to stings.Contains
 		if q.TypeStr() && other.TypeStr() {
-			return strings.Contains(q.ref.Interface().(string), targets.(string))
+			return strings.Contains(q.ref.Interface().(string), obj.(string))
 		}
 
 		// Other is non iterable, convert to iterable
 		if other.TypeSingle() {
-			other.Set([]interface{}{targets})
+			other.Set([]interface{}{obj})
 		}
 		next := other.Iter()
 		for x, ok := next(); ok; x, ok = next() {
@@ -49,7 +56,7 @@ func (q *Queryable) Contains(targets interface{}) bool {
 			} else {
 				next := q.Iter()
 				for x, ok := next(); ok; x, ok = next() {
-					if x == targets {
+					if x == obj {
 						return true
 					}
 				}
@@ -67,7 +74,7 @@ func (q *Queryable) Contains(targets interface{}) bool {
 					}
 				}
 			} else {
-				if !keys.Contains(targets) {
+				if !keys.Contains(obj) {
 					return false
 				}
 			}
@@ -78,21 +85,22 @@ func (q *Queryable) Contains(targets interface{}) bool {
 	return true
 }
 
-// ContainsAny checks if any of the given targets is found
-func (q *Queryable) ContainsAny(targets interface{}) bool {
-	other := Q(targets)
+// ContainsAny checks if any of the given obj is found.
+// ContainsAny behaves much like Contains only it allows for matching any not all.
+func (q *Queryable) ContainsAny(obj interface{}) bool {
+	other := Q(obj)
 
 	// Non iterable type
 	if q.TypeSingle() {
 
 		// Both strings - pass through to stings.Contains
 		if q.TypeStr() && other.TypeStr() {
-			return strings.Contains(q.ref.Interface().(string), targets.(string))
+			return strings.Contains(q.ref.Interface().(string), obj.(string))
 		}
 
 		// Other is non iterable, convert to iterable
 		if other.TypeSingle() {
-			other.Set([]interface{}{targets})
+			other.Set([]interface{}{obj})
 		}
 		next := other.Iter()
 		for x, ok := next(); ok; x, ok = next() {
@@ -116,7 +124,7 @@ func (q *Queryable) ContainsAny(targets interface{}) bool {
 			} else {
 				next := q.Iter()
 				for x, ok := next(); ok; x, ok = next() {
-					if x == targets {
+					if x == obj {
 						return true
 					}
 				}
@@ -133,7 +141,7 @@ func (q *Queryable) ContainsAny(targets interface{}) bool {
 				}
 			} else {
 				for _, key := range q.ref.MapKeys() {
-					if key.Interface() == targets {
+					if key.Interface() == obj {
 						return true
 					}
 				}
