b0d39c5f0b3a557736256120e9e08074913fdebe
phR0ze <prc451@gmail.com>
2019-08-16 11:39:15 -0600

    2.0.41: Adding a sub package net/agent

---
+++ pkg/net/agent/agent.go
@@ -0,0 +1,16 @@
+// Package agent provides simple agent helper functions
+package agent
+
+const (
+	// User agent strings
+	WindowsIE6     = "Windows IE 6"
+	WindowsIE7     = "Windows IE 7"
+	WindowsMozilla = "Windows Mozilla"
+	MacSafari      = "Mac Safari"
+	MacFirefox     = "Mac FireFox"
+	MacMozilla     = "Mac Mozilla"
+	LinuxMozilla   = "Linux Mozilla"
+	LinuxFirefox   = "Linux Firefox"
+	LinuxKonqueror = "Linux Konqueror"
+	Iphone         = "iPhone"
+)
