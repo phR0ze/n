756aadf99ef211177ab45f8c2353ccf3c40dcab1
phR0ze <prc451@gmail.com>
2019-09-05 01:16:28 -0600

    2.0.66: Increase extract coverage

--- pkg/futil/extract_test.go
+++ pkg/futil/extract_test.go
@@ -1,7 +1,9 @@
 package futil
 
 import (
+	"os"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/phR0ze/n/pkg/sys"
@@ -29,25 +31,57 @@ url="https://www.chromium.org/Home"
 `)
 	assert.Nil(t, err)
 
+	// invalid regex
+	{
+		match, err := ExtractString(tmpfile, `^\K`)
+		assert.Equal(t, "", match)
+		assert.Equal(t, "failed compiling regex '^\\K': error parsing regexp: invalid escape sequence: `\\K`", err.Error())
+	}
+
+	// attempt to read a write only file
+	{
+		assert.Nil(t, os.Chmod(tmpfile, 0222))
+		match, err := ExtractString(tmpfile, "")
+		assert.Equal(t, "", match)
+		assert.True(t, strings.HasPrefix(err.Error(), "failed reading the file"))
+		assert.True(t, strings.HasSuffix(err.Error(), ": permission denied"))
+		assert.Nil(t, os.Chmod(tmpfile, 0644))
+	}
+
+	// filepath is empty
+	{
+		match, err := ExtractString("", "")
+		assert.Equal(t, "", match)
+		assert.Equal(t, "empty string is an invalid path", err.Error())
+	}
+
 	// No match
-	match, err := ExtractString(tmpfile, `foobar`)
-	assert.Nil(t, err)
-	assert.Equal(t, "", match)
+	{
+		match, err := ExtractString(tmpfile, `foobar`)
+		assert.Nil(t, err)
+		assert.Equal(t, "", match)
+	}
 
 	// Single Match - whole string
-	match, err = ExtractString(tmpfile, `(?m)^(pkgver=.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, "pkgver=76.0.3809.100", match)
+	{
+		match, err := ExtractString(tmpfile, `(?m)^(pkgver=.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, "pkgver=76.0.3809.100", match)
+	}
 
 	// Single Match
-	match, err = ExtractString(tmpfile, `(?m)^pkgver=(.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, "76.0.3809.100", match)
+	{
+		match, err := ExtractString(tmpfile, `(?m)^pkgver=(.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, "76.0.3809.100", match)
+	}
 
 	// Multiple Match - only sees the first one
-	match, err = ExtractString(tmpfile, `.*pkgver=(.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, "76.0.3809.100", match)
+	{
+		match, err := ExtractString(tmpfile, `.*pkgver=(.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, "76.0.3809.100", match)
+	}
 }
 
 func TestExtractStrings(t *testing.T) {
@@ -66,30 +100,64 @@ url="https://www.chromium.org/Home"
 `)
 	assert.Nil(t, err)
 
+	// invalid regex
+	{
+		match, err := ExtractStrings(tmpfile, `^\K`)
+		assert.Equal(t, ([]string)(nil), match)
+		assert.Equal(t, "failed compiling regex '^\\K': error parsing regexp: invalid escape sequence: `\\K`", err.Error())
+	}
+
+	// attempt to read a write only file
+	{
+		assert.Nil(t, os.Chmod(tmpfile, 0222))
+		match, err := ExtractStrings(tmpfile, "")
+		assert.Equal(t, ([]string)(nil), match)
+		assert.True(t, strings.HasPrefix(err.Error(), "failed reading the file"))
+		assert.True(t, strings.HasSuffix(err.Error(), ": permission denied"))
+		assert.Nil(t, os.Chmod(tmpfile, 0644))
+	}
+
+	// filepath is empty
+	{
+		match, err := ExtractStrings("", "")
+		assert.Equal(t, ([]string)(nil), match)
+		assert.Equal(t, "empty string is an invalid path", err.Error())
+	}
+
 	// No match
-	matches, err := ExtractStrings(tmpfile, `foobar`)
-	assert.Nil(t, err)
-	assert.Nil(t, matches)
+	{
+		matches, err := ExtractStrings(tmpfile, `foobar`)
+		assert.Nil(t, err)
+		assert.Nil(t, matches)
+	}
 
 	// Single Match substring
-	matches, err = ExtractStrings(tmpfile, `(?m)^pkgver=(.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"76.0.3809.100"}, matches)
+	{
+		matches, err := ExtractStrings(tmpfile, `(?m)^pkgver=(.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"76.0.3809.100"}, matches)
+	}
 
 	// Single Match wholestring
-	matches, err = ExtractStrings(tmpfile, `(?m)^(pkgver=.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"pkgver=76.0.3809.100"}, matches)
+	{
+		matches, err := ExtractStrings(tmpfile, `(?m)^(pkgver=.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"pkgver=76.0.3809.100"}, matches)
+	}
 
 	// Multiple Match substrings
-	matches, err = ExtractStrings(tmpfile, `.*pkgver=(.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"76.0.3809.100", "foo"}, matches)
+	{
+		matches, err := ExtractStrings(tmpfile, `.*pkgver=(.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"76.0.3809.100", "foo"}, matches)
+	}
 
 	// Multiple Match whole strings
-	matches, err = ExtractStrings(tmpfile, `.*(pkgver=.*)`)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"pkgver=76.0.3809.100", "pkgver=foo"}, matches)
+	{
+		matches, err := ExtractStrings(tmpfile, `.*(pkgver=.*)`)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"pkgver=76.0.3809.100", "pkgver=foo"}, matches)
+	}
 }
 
 func TestExtractStringP(t *testing.T) {
@@ -108,33 +176,62 @@ url="https://www.chromium.org/Home"
 `)
 	assert.Nil(t, err)
 
+	// attempt to read a write only file
+	{
+		rx, err := regexp.Compile(`foobar`)
+		assert.Nil(t, err)
+		assert.Nil(t, os.Chmod(tmpfile, 0222))
+		match, err := ExtractStringP(tmpfile, rx)
+		assert.Equal(t, "", match)
+		assert.True(t, strings.HasPrefix(err.Error(), "failed reading the file"))
+		assert.True(t, strings.HasSuffix(err.Error(), ": permission denied"))
+		assert.Nil(t, os.Chmod(tmpfile, 0644))
+	}
+
+	// filepath is empty
+	{
+		rx, err := regexp.Compile(`foobar`)
+		assert.Nil(t, err)
+		match, err := ExtractStringP("", rx)
+		assert.Equal(t, "", match)
+		assert.Equal(t, "empty string is an invalid path", err.Error())
+	}
+
 	// No match
-	rx, err := regexp.Compile(`foobar`)
-	assert.Nil(t, err)
-	match, err := ExtractStringP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, "", match)
+	{
+		rx, err := regexp.Compile(`foobar`)
+		assert.Nil(t, err)
+		match, err := ExtractStringP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, "", match)
+	}
 
 	// Single Match - whole string
-	rx, err = regexp.Compile(`(?m)^(pkgver=.*)`)
-	assert.Nil(t, err)
-	match, err = ExtractStringP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, "pkgver=76.0.3809.100", match)
+	{
+		rx, err := regexp.Compile(`(?m)^(pkgver=.*)`)
+		assert.Nil(t, err)
+		match, err := ExtractStringP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, "pkgver=76.0.3809.100", match)
+	}
 
 	// Single Match
-	rx, err = regexp.Compile(`(?m)^pkgver=(.*)`)
-	assert.Nil(t, err)
-	match, err = ExtractStringP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, "76.0.3809.100", match)
+	{
+		rx, err := regexp.Compile(`(?m)^pkgver=(.*)`)
+		assert.Nil(t, err)
+		match, err := ExtractStringP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, "76.0.3809.100", match)
+	}
 
 	// Multiple Match - only sees the first one
-	rx, err = regexp.Compile(`.*pkgver=(.*)`)
-	assert.Nil(t, err)
-	match, err = ExtractStringP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, "76.0.3809.100", match)
+	{
+		rx, err := regexp.Compile(`.*pkgver=(.*)`)
+		assert.Nil(t, err)
+		match, err := ExtractStringP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, "76.0.3809.100", match)
+	}
 }
 
 func TestExtractStringsP(t *testing.T) {
@@ -153,40 +250,71 @@ url="https://www.chromium.org/Home"
 `)
 	assert.Nil(t, err)
 
+	// attempt to read a write only file
+	{
+		rx, err := regexp.Compile(`foobar`)
+		assert.Nil(t, err)
+		assert.Nil(t, os.Chmod(tmpfile, 0222))
+		match, err := ExtractStringsP(tmpfile, rx)
+		assert.Equal(t, ([]string)(nil), match)
+		assert.True(t, strings.HasPrefix(err.Error(), "failed reading the file"))
+		assert.True(t, strings.HasSuffix(err.Error(), ": permission denied"))
+		assert.Nil(t, os.Chmod(tmpfile, 0644))
+	}
+
+	// filepath is empty
+	{
+		rx, err := regexp.Compile(`foobar`)
+		assert.Nil(t, err)
+		match, err := ExtractStringsP("", rx)
+		assert.Equal(t, ([]string)(nil), match)
+		assert.Equal(t, "empty string is an invalid path", err.Error())
+	}
+
 	// No match
-	rx, err := regexp.Compile(`foobar`)
-	assert.Nil(t, err)
-	matches, err := ExtractStringsP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Nil(t, matches)
+	{
+		rx, err := regexp.Compile(`foobar`)
+		assert.Nil(t, err)
+		matches, err := ExtractStringsP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Nil(t, matches)
+	}
 
 	// Single Match substring
-	rx, err = regexp.Compile(`(?m)^pkgver=(.*)`)
-	assert.Nil(t, err)
-	matches, err = ExtractStringsP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"76.0.3809.100"}, matches)
+	{
+		rx, err := regexp.Compile(`(?m)^pkgver=(.*)`)
+		assert.Nil(t, err)
+		matches, err := ExtractStringsP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"76.0.3809.100"}, matches)
+	}
 
 	// Single Match wholestring
-	rx, err = regexp.Compile(`(?m)^(pkgver=.*)`)
-	assert.Nil(t, err)
-	matches, err = ExtractStringsP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"pkgver=76.0.3809.100"}, matches)
+	{
+		rx, err := regexp.Compile(`(?m)^(pkgver=.*)`)
+		assert.Nil(t, err)
+		matches, err := ExtractStringsP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"pkgver=76.0.3809.100"}, matches)
+	}
 
 	// Multiple Match substrings
-	rx, err = regexp.Compile(`.*pkgver=(.*)`)
-	assert.Nil(t, err)
-	matches, err = ExtractStringsP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"76.0.3809.100", "foo"}, matches)
+	{
+		rx, err := regexp.Compile(`.*pkgver=(.*)`)
+		assert.Nil(t, err)
+		matches, err := ExtractStringsP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"76.0.3809.100", "foo"}, matches)
+	}
 
 	// Multiple Match whole strings
-	rx, err = regexp.Compile(`.*(pkgver=.*)`)
-	assert.Nil(t, err)
-	matches, err = ExtractStringsP(tmpfile, rx)
-	assert.Nil(t, err)
-	assert.Equal(t, []string{"pkgver=76.0.3809.100", "pkgver=foo"}, matches)
+	{
+		rx, err := regexp.Compile(`.*(pkgver=.*)`)
+		assert.Nil(t, err)
+		matches, err := ExtractStringsP(tmpfile, rx)
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"pkgver=76.0.3809.100", "pkgver=foo"}, matches)
+	}
 }
 
 func cleanTmpDir() {
